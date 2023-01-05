4155d76ed51ba3be2d33c19409c44547a778254e
Matheus Milanese <matheus.milanese@gmail.com>
2023-01-05 09:08:04 -0300

    updadeScene

--- Game/Assets/Scripts/Gun/bulletController.cs
+++ Game/Assets/Scripts/Gun/bulletController.cs
@@ -10,8 +10,18 @@ public class bulletController : MonoBehaviour
         _rBody = GetComponent<Rigidbody2D>();
     }
 
+    private void Start() {
+        StartCoroutine("DestroyBullet");
+    }
+
     public void AddStartingForce(float forceX, float forceY){
         
         _rBody.AddForce(new Vector2(forceX, forceY));
+    }
+
+
+    private IEnumerator DestroyBullet(){
+        yield return new WaitForSeconds(15);
+        Destroy(this.gameObject);
     }
 }
